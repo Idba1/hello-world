eac3a0c1a4d3bd31944c9f132e8e4511360620cf
Idba1 <moniraislamself@gmail.com>
2024-09-29 22:46:32 +0600

    arithmatic operation with +,-,/,*,%, and lld

---
+++ C-programming/Module-2/arithmatic-operrations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter two int number: ");
+    scanf("%d %d",&a ,&b);
+    printf("sum = %d \n",a+b);
+    printf("sub = %d \n",a-b);
+    printf("mult = %d \n",a*b);
+    printf("divi = %d \n",a/b);
+    printf("mod = %d \n",a%b);
+    return 0;
+}
