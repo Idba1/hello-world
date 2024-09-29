d4aba7cf2f26413c803448e8ba5d2f811f5d9d6f
Idba1 <moniraislamself@gmail.com>
2024-09-30 00:46:58 +0600

    conditional statements

---
+++ C-programming/Module-2/conditional-statements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    int a = 2;
+    if (a >= 2)
+    {
+        a += 121;
+        printf("%d",a);
+    }
+    else
+    {
+        printf("small");
+    }
+    return 0;
+}
