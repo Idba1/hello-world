482ddd12f0b62185c582aba3a74f5b4e3b7e3c74
Idba1 <moniraislamself@gmail.com>
2024-09-29 07:16:54 +0600

    input output using scanf and printf

---
+++ C-programming/Module-1/input_output.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    long long int u;
+    char ch;
+    printf("Enter a integer value:");
+    scanf("%d", &a);
+    printf("your value is %d\n", a);
+    printf("Enter a long long integer value:");
+    scanf("%lld", &u);
+    printf("your long long int value is %lld\n", u);
+    printf("Enter a character value:");
+    scanf("\n%c", &ch);
+    printf("your long long int value is %c\n", ch);
+    return 0;
+}
