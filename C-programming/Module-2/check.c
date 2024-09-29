95c4d1ef3dd87a1575a4923096511fd1a97da9ed
Idba1 <moniraislamself@gmail.com>
2024-09-30 03:14:35 +0600

    finished module-2

---
+++ C-programming/Module-2/check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    // printf("%d\n", ++a + a++);
+    printf("%d\n", ++a);
+    printf("%d\n", a++);
+    printf("%d\n", a);
+    
+    return 0;
+}
