b8e360aba6116921fdc9140069799c06630018a7
Idba1 <moniraislamself@gmail.com>
2024-09-30 00:53:15 +0600

    if-else

---
+++ C-programming/Module-2/if-else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+    int price = 50;
+    if (price <= 50)
+    {
+        printf("Buy 4 Eggs");
+    }
+    else
+    {
+        printf("Buy 2 Eggs");
+    }
+    return 0;
+}
