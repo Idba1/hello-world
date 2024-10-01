8eed567f0418ad13776bb7da01e92fc67f426287
Idba1 <moniraislamself@gmail.com>
2024-10-01 22:09:20 +0600

    basic nested for loop

---
+++ C-programming/Module-3/nested-loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    // i = 5;
+    // j = 7;
+    for (i = 0; i < 5; i++)
+    {
+        for ( j = 0; j < 7; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
