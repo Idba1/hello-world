b569e3f8ea91846692adb65d72bbfebba7ea42d2
Idba1 <moniraislamself@gmail.com>
2024-10-06 05:09:33 +0600

    conceptual-2

---
+++ C-programming/Conceptual-2/even-and-minus-one.c
@@ -0,0 +1,20 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int N, count = 0;
+//     scanf("%d", &N);
+//     for (int i = 1; i < N; i++)
+//     {
+//         if (i % 2 == 0)
+//         {
+//             count+=1;
+//             printf("%d\n",i);
+//         }
+//         else
+//         {
+//             printf("-1\n");
+//         }
+//     }
+
+//     return 0;
+// }
