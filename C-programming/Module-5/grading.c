01b39eac1e21792b8edfe5701912a8515fa0ea1e
Idba1 <moniraislamself@gmail.com>
2024-10-06 06:23:09 +0600

    ascii value

--- C-programming/Module-5/grading.c
+++ C-programming/Module-5/grading.c
@@ -5,43 +5,53 @@ int main()
     scanf("%d", &marks);
     if (marks > 32 && marks < 101)
     {
-        if (marks >= 80)
+        if (marks >= 80 && marks < 101)
         {
-            printf("A+");
+            printf("A+\n");
+            if (marks >= 90)
+            {
+                printf("got scholarship\n");
+            }
+            else
+            {
+                printf("not got scholarship\n");
+            }
         }
         else if (marks > 69 && marks < 80)
         {
-            printf("A");
+            printf("A\n");
         }
         else if (marks > 59 && marks < 70)
         {
-            printf("A-");
+            printf("A-\n");
         }
         else if (marks > 49 && marks < 60)
         {
-            printf("B");
+            printf("B\n");
         }
         else if (marks > 39 && marks < 50)
         {
-            printf("C");
+            printf("C\n");
         }
         else if (marks > 32 && marks < 40)
         {
-            printf("D");
+            printf("D\n");
         }
         else
         {
             printf("Enter a valid number 0-100");
         }
+        printf("Pass\n");
     }
     else if (marks < 33 && marks >= 0)
     {
-        printf("F");
+        printf("F\n");
+        printf("Fail\n");
     }
 
     else
     {
-        printf("enter a valid marks 0-100");
+        printf("enter a valid marks 0-100\n");
     }
     return 0;
 }
