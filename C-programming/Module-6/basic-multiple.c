1f48b9a8c087eb870510a627bb8670b455f9d332
Idba1 <moniraislamself@gmail.com>
2024-10-07 18:41:17 +0600

    update basic multiple

--- C-programming/Module-6/basic-multiple.c
+++ C-programming/Module-6/basic-multiple.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    for (int i = 1; i <= 10; i++)
+    int m,n;
+    scanf("%d %d",&m, &n); 
+    for (int i = 1; i <= m; i++)
     {
-        printf("%d * %2d = %2d\n", n, i, n * i);
+        for (int j = 1; j <= n; j++)
+        {
+            printf("%d * %2d = %2d\n", i, j, i * j);
+        }
+        printf("\n");
     }
 
     return 0;
