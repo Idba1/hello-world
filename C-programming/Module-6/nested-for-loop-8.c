0b42a4044fa4c50d947c6c52bd7401062e6e9b4d
Idba1 <moniraislamself@gmail.com>
2024-10-08 09:50:47 +0600

    nested-for-loop-6

--- C-programming/Module-6/nested-for-loop-8.c
+++ C-programming/Module-6/nested-for-loop-8.c
@@ -3,10 +3,9 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
+    for (int i = n; i >= 1; i--)
     {
-
-        for (int j = n; j >= i; j--)
+        for (int j = i; j >= 1; j--)
         {
             printf("%d", j);
         }
