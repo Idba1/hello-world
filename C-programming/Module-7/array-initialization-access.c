4f8ff6d43bc41ef03a1066a539bcd23961638baf
Idba1 <moniraislamself@gmail.com>
2024-10-09 04:27:45 +0600

    array-initializaztion-access-memory

---
+++ C-programming/Module-7/array-initialization-access.c
@@ -0,0 +1,7 @@
+int arr[5] = {3, 4, 5, 6, 7};
+int arr[] = {4, 5, 6};
+int arr[4] = {0};      // dhore nibe sobgulO index er value 0
+int arr[5] = {10, 20}; //{10,20,0,0,0}
+// int arr[2] += 5;
+// arr[5]=arr[0],arr[1],arr[2],arr[3],arr[4],
+// memory location : 530, 534, 538, 542, 546
