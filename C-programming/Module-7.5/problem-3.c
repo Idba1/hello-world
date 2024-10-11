6f43ba3c816bb370cbb336f7eda0f0724cc422ff
Idba1 <moniraislamself@gmail.com>
2024-10-11 08:19:20 +0600

    problem-3

---
+++ C-programming/Module-7.5/problem-3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+        if (A[i] > 0)
+        {
+            A[i] = 1;
+        }
+        else if (A[i] < 0)
+        {
+            A[i] = 2;
+        }
+        else if (A[i] = 0)
+        {
+            A[i] = 0;
+        }
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
