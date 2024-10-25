9aefa0543840caa43a9c9b89ba8f86593686affc
Idba1 <moniraislamself@gmail.com>
2024-10-26 01:39:55 +0600

    fgets and fputs

--- C-programming/Module-10/input-output.c
+++ C-programming/Module-10/input-output.c
@@ -7,12 +7,16 @@ int main()
     // char str[20];
     // scanf("%s", str);
     // printf("first string is %s\n", str);
-     
+
     // getchar(); // Newline consume korte eta add kora
 
-    char strLine[30];
-    scanf("%[^\n]s", strLine);
-    printf("second string in %s\n", strLine);
+    // char strLine[30];
+    // scanf("%[^\n]s", strLine);
+    // printf("second string in %s\n", strLine);
+
+    char strLib[30];
+    fgets(strLib, sizeof(strLib), stdin);
+    fputs(strLib, stdout);
 
     return 0;
 }
