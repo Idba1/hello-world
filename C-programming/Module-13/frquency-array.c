9a2f7172413754d5b8a7c2a8c542e4bfdfa10e45
Idba1 <moniraislamself@gmail.com>
2024-10-26 23:58:16 +0600

    update frequency array

--- C-programming/Module-13/frquency-array.c
+++ C-programming/Module-13/frquency-array.c
@@ -20,6 +20,21 @@ int main()
     {
         printf("%d %d\n", i, f[i]);
     }
+    int m;
+    scanf("%d", &m);
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        scanf("%d", &x);
+        if (f[x] == 1)
+        {
+            printf("ache...\n");
+        }
+        else
+        {
+            printf("nai\n");
+        }
+    }
 
     return 0;
 }
