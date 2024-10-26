ad78f747d76cc8d6d2442c58adaab7f0182cf9d2
Idba1 <moniraislamself@gmail.com>
2024-10-26 23:53:40 +0600

    frequency array

---
+++ C-programming/Module-13/frquency-array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n + 5];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    int f[100000] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        f[a[i]] = 1;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d %d\n", i, f[i]);
+    }
+
+    return 0;
+}
