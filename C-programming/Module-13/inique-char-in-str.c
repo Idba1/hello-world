58b5218bc96c47981c3f8a3b925aecacb523a86e
Idba1 <moniraislamself@gmail.com>
2024-10-27 20:56:05 +0600

    unique characters in a str

---
+++ C-programming/Module-13/inique-char-in-str.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+    char str[100] = "apple";
+    int f[26] = {0};
+    int len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        char ch = str[i];
+        int index = ch - 'a';
+        f[index] = 1;
+    }
+    int count = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        count += f[i];
+        if (f[i] == 1)
+        {
+            printf("%c %d\n", i + 'a', f[i]);
+        }
+    }
+    printf("%d\n", count);
+    return 0;
+}
