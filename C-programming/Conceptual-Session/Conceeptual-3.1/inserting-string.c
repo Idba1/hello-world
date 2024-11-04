caf3453fcd184041d7c2c80ae9a75140a8482441
Idba1 <moniraislamself@gmail.com>
2024-11-04 08:44:50 +0600

    string insertion

--- C-programming/Conceptual-Session/Conceeptual-3.1/inserting-string.c
+++ C-programming/Conceptual-Session/Conceeptual-3.1/inserting-string.c
@@ -5,6 +5,19 @@
 
 int main()
 {
-
+    char word[100];
+    scanf("%s", word);
+    int len = strlen(word);
+    int position;
+    scanf("%d", &position);
+    for (int i = len; i >= position-1; i--)
+    {
+        word[i + 1] = word[i];
+    }
+    char insertChar;
+    getchar();
+    scanf("%c", &insertChar);
+    word[position-1] = insertChar;
+    printf("%s", word);
     return 0;
 }
