a3fed203d8af7facdb69ace686f9a15509cefa6b
Idba1 <moniraislamself@gmail.com>
2024-11-08 12:19:52 +0600

    jadu matrix --> a square matrix

---
+++ C-programming/Module-20/Final-exam/jadu-matrix.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int f[100000];
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    if (n != m)
+    {
+        printf("NO\n");
+        return 0;
+    }
+
+    int arr[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int flag = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (i == j)
+            {
+                if (arr[i][j] != 1)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            else if (i + j == n - 1)
+            {
+                if (arr[i][j] != 1)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            else
+            {
+                if (arr[i][j] != 0)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 0)
+            {
+                break;
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
