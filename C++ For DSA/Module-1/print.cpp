3c641fc607b523963a94aba0548dd574e0814a60
Idba1 <moniraislamself@gmail.com>
2024-11-17 17:58:58 +0600

    print variable

--- C++ For DSA/Module-1/print.cpp
+++ C++ For DSA/Module-1/print.cpp
@@ -2,5 +2,13 @@
 int main()
 {
     std::cout << "hello C++" << std::endl;
+    int a = 9;
+    char c = 'B';
+    double d = 33.444;
+    std::cout << a << std::endl
+              << c << std::endl
+              << d << std::endl;
+    int e = 23;
+    std::cout << "age = " << e << std::endl;
     return 0;
 }
