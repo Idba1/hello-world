966f79598660f1887676d8bb25ba2283a086c66f
Idba1 <moniraislamself@gmail.com>
2024-11-17 18:38:03 +0600

    if else statement

---
+++ C++ For DSA/Module-1/if-else.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 23;
+    if (x % 2 == 0)
+    {
+        cout << "even" << endl;
+    }
+    else
+    {
+        cout << "odd" << endl;
+    }
+    return 0;
+}
