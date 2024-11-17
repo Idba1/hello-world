b628839e1ef447a9576a7efeea61b770333a4b68
Idba1 <moniraislamself@gmail.com>
2024-11-17 18:20:17 +0600

    ascii value print

---
+++ C++ For DSA/Module-1/ascii-value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a = 'a';
+    cout << (int)a << endl; // type cast(int)
+    char c = 'c';
+    int ascii = c;
+    cout << ascii << endl;
+    return 0;
+}
