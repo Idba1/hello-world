0348a52364754554677b601cab33f4f0d0fbbb82
Idba1 <moniraislamself@gmail.com>
2024-11-17 23:36:03 +0600

    dynamic value

---
+++ C++ For DSA/Module-2/dynamic-value.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 19;
+    cout << x << endl;
+    int *p = new int;
+    *p = 1999;
+    cout << *p << endl;
+    return 0;
+}
