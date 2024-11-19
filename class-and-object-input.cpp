b205e89b55d209079a7d74019c0325811d6aae5d
Idba1 <moniraislamself@gmail.com>
2024-11-20 00:27:06 +0600

    class-and-object-input with space in char

--- class-and-object-input.cpp
+++ class-and-object-input.cpp
@@ -10,11 +10,16 @@ public:
 };
 int main()
 {
-    Student idba,rifa;
+    Student idba, rifa, anisha;
     cin >> idba.name >> idba.id >> idba.gpa;
     cin >> rifa.name >> rifa.id >> rifa.gpa;
 
+    cin.ignore(); //ignore new line before getline
+    cin.getline(anisha.name, 100); // for space in name
+    cin >> anisha.id >> anisha.gpa;
+
     cout << idba.name << " " << idba.id << " " << idba.gpa << endl;
     cout << rifa.name << " " << rifa.id << " " << rifa.gpa << endl;
+    cout << anisha.name << " " << anisha.id << " " << anisha.gpa << endl;
     return 0;
 }
