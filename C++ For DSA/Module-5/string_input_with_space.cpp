c75f8cb4ec8499acfbae4e8664039e3564c954f3
Idba1 <moniraislamself@gmail.com>
2024-11-24 03:31:14 +0600

    String input with spaces

---
+++ C++ For DSA/Module-5/string_input_with_space.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    string s;
+    cin.ignore();
+    getline(cin, s);
+    cout << x << endl;
+    cout << s << endl;
+    string s2;
+    cin >> s2;
+    cout << s2 << endl;
+    return 0;
+}
