412a3b4399fd99c87db19e2cda0df788744087e6
Idba1 <moniraislamself@gmail.com>
2024-11-23 20:20:31 +0600

    capacity and max_size

---
+++ C++ For DSA/Module-5/stringCapacity/maxSize.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "green red yellow blue orange";
+    cout << s1.capacity() << endl;
+    cout << s1.max_size() << endl;
+    return 0;
+}
