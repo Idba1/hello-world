3bd9d2a4e29cec4acc2cab18319351e699f599ba
Idba1 <moniraislamself@gmail.com>
2024-11-24 02:03:02 +0600

    replace

---
+++ C++ For DSA/Module-5/stringModifiers/replace.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "hello world";
+    cout << s << endl;
+    s.replace(6, 0, "Idba ");
+    cout << s << endl;
+    return 0;
+}
