01beedde0fdeb3ccca4a6bada8fc36c97236bf72
Idba1 <moniraislamself@gmail.com>
2024-11-24 02:09:05 +0600

    insert function

--- C++ For DSA/Module-5/stringModifiers/replace.cpp
+++ C++ For DSA/Module-5/stringModifiers/replace.cpp
@@ -5,7 +5,9 @@ int main()
 {
     string s = "hello world";
     cout << s << endl;
-    s.replace(6, 0, "Idba ");
+    s.replace(6, 5, "Idba ");
+    cout << s << endl;
+    s.insert(11, "Islam");
     cout << s << endl;
     return 0;
 }
