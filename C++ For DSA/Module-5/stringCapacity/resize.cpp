eac79d1f2896eb42681daa0cbfb777884934c002
Idba1 <moniraislamself@gmail.com>
2024-11-23 20:33:18 +0600

    resize fun

---
+++ C++ For DSA/Module-5/stringCapacity/resize.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1 = "hello";
+    s1.resize(2);
+    cout << s1 << endl;
+    s1.resize(12, 'i');
+    cout << s1 << endl;
+    return 0;
+}
