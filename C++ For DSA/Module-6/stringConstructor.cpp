f017bcf1090e48b12111875d38bea982cbeeae4b
Idba1 <moniraislamself@gmail.com>
2024-11-25 00:33:22 +0600

    string condtructor

---
+++ C++ For DSA/Module-6/stringConstructor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "hello";
+    // way_1
+    // string s("hello");
+
+    // way_2
+    // string s("hello", 2); //2 size obdhi print hobe
+
+    // way_3
+    // string s = "hello world";
+    // string t(s, 4); // 4 size er por theke print hobe
+
+    // way_4
+    string s(5, 'i'); // 5 ta i print
+    cout << s << endl;
+    return 0;
+}
