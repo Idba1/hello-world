c6f9677c3cd3789d79157768b031ba8793400ead
Idba1 <moniraislamself@gmail.com>
2024-11-25 00:34:56 +0600

    range based for loop

---
+++ C++ For DSA/Module-6/range_based_foor_loop.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin, s);
+    // for (int i = 0; i < s.length(); i++)
+    // {
+    //     cout << s[i] << endl;
+    // }
+    for (char c : s)
+    {
+        cout << c << endl;
+    }
+    return 0;
+}
