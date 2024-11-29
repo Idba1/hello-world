42a7da681969557a510626d65e42f42ddef22f2b
Idba1 <moniraislamself@gmail.com>
2024-11-29 22:50:16 +0600

    update changes

---
+++ C++ For DSA/Module6.5/problem-3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    string flag = "EGYPT";
+
+    for (int i = 0; i < s.length(); i++)
+    {
+       
+    }
+
+    return 0;
+}
+
+
