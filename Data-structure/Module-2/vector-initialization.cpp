2c6402a74e6782792db50aeb4cc58a42cc9fc1bf
Idba1 <moniraislamself@gmail.com>
2024-12-09 06:13:34 +0600

    vector initialization type 2

--- Data-structure/Module-2/vector-initialization.cpp
+++ Data-structure/Module-2/vector-initialization.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main()
 {
     //vector initialization type 1
-    vector<int> v;
+    // vector<int> v;
+    // cout << v.size() << endl;
+
+    //vector initialization type 2
+    vector<int> v(100);
     cout << v.size() << endl;
     return 0;
 }
