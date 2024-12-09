b4d15154261d68290da1478a57bc4bd783ead686
Idba1 <moniraislamself@gmail.com>
2024-12-09 06:15:50 +0600

    vector initialization type 3

--- Data-structure/Module-2/vector-initialization.cpp
+++ Data-structure/Module-2/vector-initialization.cpp
@@ -2,12 +2,20 @@
 using namespace std;
 int main()
 {
-    //vector initialization type 1
-    // vector<int> v;
-    // cout << v.size() << endl;
+    // vector initialization type 1
+    //  vector<int> v;
+    //  cout << v.size() << endl;
 
-    //vector initialization type 2
-    vector<int> v(100);
-    cout << v.size() << endl;
+    // vector initialization type 2
+    //  vector<int> v(100);
+    //  cout << v.size() << endl;
+
+    // vector initialization type 3
+    vector<int> v(5, 0);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+
     return 0;
 }
