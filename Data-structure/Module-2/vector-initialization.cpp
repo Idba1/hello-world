1923146aeb43a1d9c04d9847ec766a178fcf86e2
Idba1 <moniraislamself@gmail.com>
2024-12-09 06:18:04 +0600

    vector initialization type 4

--- Data-structure/Module-2/vector-initialization.cpp
+++ Data-structure/Module-2/vector-initialization.cpp
@@ -11,10 +11,18 @@ int main()
     //  cout << v.size() << endl;
 
     // vector initialization type 3
-    vector<int> v(5, 0);
-    for (int i = 0; i < v.size(); i++)
+    // vector<int> v(5, 0);
+    // for (int i = 0; i < v.size(); i++)
+    // {
+    //     cout << v[i] << endl;
+    // }
+
+    // vector initialization type 4
+    vector<int> v1(5, 1);
+    vector<int> v2(v1);
+    for (int i = 0; i < v2.size(); i++)
     {
-        cout << v[i] << endl;
+        cout << v2[i] << endl;
     }
 
     return 0;
