7e3a634802b08600b905f2c11126b7dd96497e87
Idba1 <moniraislamself@gmail.com>
2024-12-09 14:07:38 +0600

    insert and erase

--- Data-structure/Module-2/vector-modifiers.cpp
+++ Data-structure/Module-2/vector-modifiers.cpp
@@ -21,12 +21,46 @@ int main()
     {
         cout << x << endl;
     }
-    
+
     v2.push_back(10);
     cout << "after push" << endl;
     for (int x : v2)
     {
         cout << x << endl;
     }
+
+    // insert
+    v2.insert(v2.begin() + 2, 500);
+    cout << "after insert" << endl;
+    for (int x : v2)
+    {
+        cout << x << endl;
+    }
+
+    // insert multiple value
+    vector<int> v3 = {100, 200, 400, 300};
+    v2.insert(v2.begin() + 3, v3.begin(), v3.end());
+    cout << "after insert multiple value" << endl;
+    for (int x : v2)
+    {
+        cout << x << endl;
+    }
+
+    // erase
+    v2.erase(v2.begin() + 2);
+    cout << "after erase 3rd value" << endl;
+    for (int x : v2)
+    {
+        cout << x << endl;
+    }
+
+    // erase multiple value
+    v2.erase(v2.begin() + 2, v2.begin() + 6);
+    cout << "after erase multiple value" << endl;
+    for (int x : v2)
+    {
+        cout << x << endl;
+    }
+
     return 0;
 }
