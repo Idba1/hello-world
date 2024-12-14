f327c1d7439efc49dd8730af23df2b8f8de37546
Idba1 <moniraislamself@gmail.com>
2024-12-14 18:13:56 +0600

    Quadratic complexity O(N*N)

--- Data-structure/Module-1/Quadratic_complexity.cpp
+++ Data-structure/Module-1/Quadratic_complexity.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) // O(N*N) Complexity
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout << "Hello" << endl;
+        }
+    }
 
     return 0;
 }
