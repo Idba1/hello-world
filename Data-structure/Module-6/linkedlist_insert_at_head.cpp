586b79e646e397644ae6f43a4149262cdbb7e008
Idba1 <moniraislamself@gmail.com>
2024-12-16 05:38:49 +0600

    linkedlist insert in head

---
+++ Data-structure/Module-6/linkedlist_insert_at_head.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(Node *&head, int value)
+{
+    Node *newNode = new Node(value);
+    newNode->next = head;
+    head = newNode;
+};
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = new Node(100);
+    Node *a = new Node(200);
+    Node *b = new Node(300);
+    Node *c = new Node(400);
+
+    head->next = a;
+    a->next = b;
+    b->next = c;
+
+    insert_at_head(head, 6000);
+    print_linked_list(head);
+
+    return 0;
+}
