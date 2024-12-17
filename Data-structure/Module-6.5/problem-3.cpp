221e1711ecc9a9f0a8c4f987934953a847956c77
Idba1 <moniraislamself@gmail.com>
2024-12-17 18:12:37 +0600

    problem 3 print middle point

---
+++ Data-structure/Module-6.5/problem-3.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+void print_middle(Node *head)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    Node *slow = head;
+    Node *fast = head;
+    Node *prev = NULL;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if (fast == NULL)
+    {
+        cout << prev->value << " " << slow->value << endl;
+    }
+    else
+    {
+        cout << slow->value << endl;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+
+    print_middle(head);
+
+    return 0;
+}
