b3f5011aa966d395ce39b4c4167ee761b2152006
Idba1 <moniraislamself@gmail.com>
2024-12-17 22:43:24 +0600

    deleted at head

---
+++ Data-structure/Module-7/deleted_head.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+
+    // side case
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = tail->next;
+};
+
+void deleted_at_head(Node *&head)
+{
+    Node *deletedNode = head;
+    head = head->next;
+    delete deletedNode;
+}
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << endl;
+        temp = temp->next;
+    }
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true) // complexity O(N)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value); // complexity O(1)
+    }
+
+    deleted_at_head(head);
+    print_linked_list(head);
+    deleted_at_head(head);
+    print_linked_list(head);
+
+    return 0;
+}
