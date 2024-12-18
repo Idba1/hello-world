247c3860aec3158b4654f92c3d40fbe6570162de
Idba1 <moniraislamself@gmail.com>
2024-12-18 21:28:24 +0600

    print descending order

---
+++ Data-structure/Module-7.5/problem-4.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+
+    // side case
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = tail->next;
+};
+
+void sort_linked_list(Node *&head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->value < j->value) 
+            {
+                swap(i->value, j->value);
+            }
+        }
+    }
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value <<" ";
+        temp = temp->next;
+    }
+    // cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true) // complexity O(N)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value); // complexity O(1)
+    }
+
+    sort_linked_list(head);
+    print_linked_list(head);
+
+    return 0;
+}
