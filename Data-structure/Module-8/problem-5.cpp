6acf5269f4da34f1532695bf7a8152fbeb19c851
Idba1 <moniraislamself@gmail.com>
2024-12-19 23:32:48 +0600

    remove dublicate

---
+++ Data-structure/Module-8/problem-5.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+void remove_duplicate(Node *&head)
+{
+    Node *current = head;
+    while (current != NULL)
+    {
+        Node *prev = current;
+        Node *temp = current->next;
+
+        while (temp != NULL)
+        {
+            if (temp->value == current->value)
+            {
+                prev->next = temp->next;
+                delete temp;
+                temp = prev->next;
+            }
+            else
+            {
+                prev = temp;
+                temp = temp->next;
+            }
+        }
+        current = current->next;
+    }
+}
+
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL, *tail = NULL;
+
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, value);
+    }
+    remove_duplicate(head);
+    print_linked_list(head);
+
+    return 0;
+}
