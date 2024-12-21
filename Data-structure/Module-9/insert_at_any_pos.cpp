65829ed41f0ab112dee88dba623266942d52dc36
Idba1 <moniraislamself@gmail.com>
2024-12-21 22:35:59 +0600

    doubly lined list insert at any pos

---
+++ Data-structure/Module-9/insert_at_any_pos.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void insert_at_any_pos(Node *&head, int idx, int value)
+{
+    Node *newNode = new Node(value);
+    Node *temp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next->prev = newNode;
+    temp->next = newNode;
+    newNode->prev = temp;
+
+    // cout << temp->value << endl;
+}
+
+void print_forward(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node *&tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    a->next = tail;
+    tail->prev = a;
+
+    insert_at_any_pos(head, 1, 1000);
+    insert_at_any_pos(head, 2, 2000);
+    print_forward(head);
+
+    return 0;
+}
