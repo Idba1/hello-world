515b69f7d4a08ba6d22f2957296d0431d65b332e
Idba1 <moniraislamself@gmail.com>
2024-12-23 06:02:24 +0600

    copy a in list from arr,vector,stl list

--- Data-structure/Module-10/stl_list.cpp
+++ Data-structure/Module-10/stl_list.cpp
@@ -2,10 +2,20 @@
 using namespace std;
 int main()
 {
-    list<int> list(10, 3);
-    for (auto it = list.begin(); it != list.end(); it++)
+    list<int> lst(10, 3);
+    // list<int> lst2(lst);
+    // int arr[] = {1, 2, 3, 4, 5};
+    vector<int> v = {22, 33, 45, 76};
+    list<int> lst2(v.begin(), v.end());
+
+    for (auto it = lst.begin(); it != lst.end(); it++)
     {
         cout << *it << endl;
+    }
+
+    for (int value : lst2)
+    {
+        cout << value << endl;
     }
 
     return 0;
