d868c7f29d81c2e5099436a95f8dba0d504a1bab
Idba1 <moniraislamself@gmail.com>
2025-01-09 19:45:30 +0600

    Implement Queue using STL list

---
+++ Data-structure/Module-14/implement_queue_using_list.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myQueue
+{
+public:
+    list<int> l;
+
+    void push(int value) // O(1)
+    {
+        l.push_back(value);
+    }
+    void pop() // O(1)
+    {
+        l.pop_front();
+    }
+    int front() // O(1)
+    {
+        return l.front();
+    }
+    int back() // O(1)
+    {
+        return l.back();
+    }
+    int size() // O(1)
+    {
+        return l.size();
+    }
+    bool empty() // O(1)
+    {
+        return l.empty();
+    }
+};
+
+int main()
+{
+    myQueue q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+
+    cout << q.front() << " " << q.back() << " " << q.size() << endl;
+
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    return 0;
+}
