401a5eac2420a6ed1fc774ec932b8eef5ad97db3
Idba1 <moniraislamself@gmail.com>
2025-01-09 19:03:03 +0600

    Implement Queue using Singly linked list create function

---
+++ Data-structure/Module-14/implement_queue_using_singly.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class myQueue
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int sz = 0;
+
+    void push(int value) // O(1)
+    {
+        sz++;
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+    void pop() // O(1)
+    {
+        sz--;
+        Node *deletedNode = head;
+        head = head->next;
+        delete deletedNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+    }
+    int front() // O(1)
+    {
+        return head->val;
+    }
+    int back() // O(1)
+    {
+        return tail->val;
+    }
+    int size() //O(1)
+    {
+        return sz;
+    }
+    bool empty() //O(1)
+    {
+        if (head == NULL)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        // return head == NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
