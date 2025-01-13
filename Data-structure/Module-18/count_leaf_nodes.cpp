047fc8359f62ad6f1f682d20107afa8eb1e0b056
Idba1 <moniraislamself@gmail.com>
2025-01-13 18:41:05 +0600

    Count leaf nodes in a binary tree

---
+++ Data-structure/Module-18/count_leaf_nodes.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int value;
+    cin >> value;
+    Node *root;
+    if (value == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(value);
+    }
+
+    queue<Node *> q;
+    if (root != NULL)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        // step1: ber kore ana
+        Node *p = q.front();
+        q.pop();
+        // step2: oi Node ke niye kaj kora
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+        {
+            myLeft = NULL;
+        }
+        else
+        {
+            myLeft = new Node(l);
+        }
+
+        if (r == -1)
+        {
+            myRight = NULL;
+        }
+        else
+        {
+            myRight = new Node(r);
+        }
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        // step3: children goloke push kora
+        if (p->left != NULL)
+        {
+            q.push(p->left);
+        }
+        if (p->right != NULL)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+int count_leaf_nodes(Node *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+
+    int l = count_leaf_nodes(root->left);
+    int r = count_leaf_nodes(root->right);
+    return l + r;
+}
+
+int main()
+{
+    Node *root = input_tree();
+    int count = count_leaf_nodes(root);
+    cout << count << endl;
+    return 0;
+}
