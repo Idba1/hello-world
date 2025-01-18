872122597451941ebbc93e1c0b74d2276c4282c5
Idba1 <moniraislamself@gmail.com>
2025-01-19 03:00:44 +0600

    insert in bst

---
+++ Data-structure/Module-21/insert_in_BST.cpp
@@ -0,0 +1,137 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *left;
+    Node *right;
+    Node(int value)
+    {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *input_tree()
+{
+    int value;
+    cin >> value;
+    Node *root;
+    if (value == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(value);
+    }
+
+    queue<Node *> q;
+    if (root != NULL)
+    {
+        q.push(root);
+    }
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+        Node *myLeft, *myRight;
+        if (l == -1)
+        {
+            myLeft = NULL;
+        }
+        else
+        {
+            myLeft = new Node(l);
+        }
+
+        if (r == -1)
+        {
+            myRight = NULL;
+        }
+        else
+        {
+            myRight = new Node(r);
+        }
+
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (p->left != NULL)
+        {
+            q.push(p->left);
+        }
+        if (p->right != NULL)
+        {
+            q.push(p->right);
+        }
+    }
+    return root;
+}
+
+void level_order(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+        cout << f->value << " ";
+
+        if (f->left != NULL)
+        {
+            q.push(f->left);
+        }
+        if (f->right != NULL)
+        {
+            q.push(f->right);
+        }
+    }
+}
+
+void insert(Node *&root, int value)
+{
+    if (root == NULL)
+    {
+        root = new Node(value);
+    }
+    if (root->value > value)
+    {
+        if (root->left == NULL)
+        {
+            root->left = new Node(value);
+        }
+        else
+        {
+            insert(root->left, value);
+        }
+    }
+    else
+    {
+        if (root->right == NULL)
+        {
+            root->right = new Node(value);
+        }
+        else
+        {
+            insert(root->right, value);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = input_tree();
+    int value;
+    cin >> value;
+    insert(root, value);
+    level_order(root);
+    return 0;
+}
