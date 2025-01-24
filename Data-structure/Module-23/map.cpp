d10bf46ca33684c5d3eb0475f56543080f71eb4d
Idba1 <moniraislamself@gmail.com>
2025-01-24 11:01:54 +0600

    value check in map

--- Data-structure/Module-23/map.cpp
+++ Data-structure/Module-23/map.cpp
@@ -7,11 +7,17 @@ int main()
     mp["rifa"] = 8;
     mp["rafa"] = 10; // logN
     mp["anisha"] = 7;
+    mp["Tasin"] = 0;
     // cout << mp["rifa"] << endl;
     // cout << mp["anisha"] << endl;
     for (auto it = mp.begin(); it != mp.end(); it++) // NlogN
     {
         cout << it->first << " " << it->second << endl; // logN
     }
+
+    if (mp.count("sing"))
+        cout << "there" << endl;
+    else
+        cout << "No" << endl;
     return 0;
 }
