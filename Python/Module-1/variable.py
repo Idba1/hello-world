c397fa55baa75a2700bde461998a06ff6f1fa854
Idba1 <monirsislamself@gmail.com>
2025-03-23 00:35:10 -0700

    Basic Data Types and Variables in python

---
+++ Python/Module-1/variable.py
@@ -0,0 +1,25 @@
+age = 5
+print(age)
+
+interest_rate = 2.4
+print(interest_rate)
+
+str = "String"
+print(str)
+
+is_single = True
+is_Sleep = False
+
+sum = (age*interest_rate)
+print(sum)
+print(type(sum))
+# print(type(age))
+""" 
+Multiline comment-> alt+shift+A
+single line-> ctrl + /
+"""
+
+name = "monira"
+print("Monira" + " " + "Islam")
+text = f"hello from {name}"
+print(text)
