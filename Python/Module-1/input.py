6a93c492ce5e4dfdf285076cd151000fbd985dba
Idba1 <monirsislamself@gmail.com>
2025-03-23 01:02:22 -0700

    Basic Input and Output and Typecasting in python

---
+++ Python/Module-1/input.py
@@ -0,0 +1,21 @@
+print("now i need money")
+money = input()
+print(money)
+
+money2 = input('Give me some money: ')
+
+first_money = input('friend 1 give me some tk: ')
+first_money_int = int(first_money)  # type casting
+first_money_flot = float(first_money)
+print(type(first_money_flot))
+second_money = input('friend 2 give me some tk: ')
+second_money_int = int(second_money)
+total_str = first_money+second_money
+total_int = first_money_int+second_money_int
+print(type(first_money_int))
+print(type(first_money))
+print(total_str)
+print(total_int)
+print(first_money_flot+second_money_int)
+str2 = str(first_money_flot)
+print(type(str2))
