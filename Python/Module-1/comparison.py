bf77aa50e24ad9149009111f6fd106179a12b86b
Idba1 <monirsislamself@gmail.com>
2025-03-23 01:13:48 -0700

    Basic Operators and comparison

---
+++ Python/Module-1/comparison.py
@@ -0,0 +1,15 @@
+#  >, <, >=, <=, ==, !==
+# no ++ --
+
+num1 = 23
+num2 = 4
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == 23)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+power = 5**3
+print(power)
