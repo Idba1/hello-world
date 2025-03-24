b16ed61244b24154e38b7eb7c1cd1d30ad2b644a
Idba1 <monirsislamself@gmail.com>
2025-03-24 19:53:40 +0600

    Introduction to functions in python

---
+++ Python/Module-2/function.py
@@ -0,0 +1,20 @@
+""" 
+def
+"""
+
+
+def double_it(num):
+    result = num*2
+    print(result)
+    return result
+
+
+double_it(7)
+
+
+def sum(num1, num2):
+    total = num1+num2
+    return total
+
+
+print(sum(23, 2))
