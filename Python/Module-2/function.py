6abc5df79c68aab69aab253b046ad80513784366
Idba1 <moniraislamself@gmail.com>
2025-03-28 12:21:59 +0600

    Introduction to functions in python

--- Python/Module-2/function.py
+++ Python/Module-2/function.py
@@ -1,20 +1,24 @@
-""" 
-def
-"""
-
-
-def double_it(num):
-    result = num*2
-    print(result)
-    return result
-
-
-double_it(7)
-
-
 def sum(num1, num2):
     total = num1+num2
     return total
 
 
 print(sum(23, 2))
+
+def double_it(num):
+    result = num * 2
+    print('inside the function.py file', result)
+    return result
+
+double_it(8)
+double_it(88)
+
+def sum(num1, num2):
+    result = num1 + num2
+    return result
+
+total = sum(44, 39)
+print('total value', total)
+
+final = double_it(total)
+print('final value', final)
