64ad6267ae995f38ea47fc6174e3329d29b471a8
Idba1 <moniraislamself@gmail.com>
2025-04-04 22:22:00 +0600

    Set and set methods in python

--- Python/Module-3/tuple.py
+++ Python/Module-3/tuple.py
@@ -2,7 +2,7 @@ def multiple():
     return 3, 4
 # print(multiple())
 things = 'pen', 'tripod', 'water bottle', 'charger', 'phone', 'web cam', 'sunglass'
-# print(type(things))
+print(type(things))
 print(things[0])
 print(things[-2])
 print(things[3:6])
@@ -20,7 +20,7 @@ items = ('book', 'monitor')
 # ignore
 print(len(things))
 mega = ([2, 3,4], [6,8,9,5])
-# print(type(mega))
+print(type(mega))
 print(mega[0])
 mega[0][1]=666
 print(mega)
