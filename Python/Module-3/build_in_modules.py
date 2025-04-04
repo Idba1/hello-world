a21beefcc6724e89ff80a248f405fc2d4da33bd8
Idba1 <moniraislamself@gmail.com>
2025-04-04 22:49:16 +0600

    built in modules time, math, random, etc

---
+++ Python/Module-3/build_in_modules.py
@@ -0,0 +1,11 @@
+from math import *
+from random import *
+from time import sleep
+result = ceil(4.00001)
+result2 = floor(4.9999)
+print(result)
+print(result2)
+print(random())
+print(randint(1,100))
+sleep(4)
+print(choice(['sakib', 'mash', 'mush', 'rid', 'musta']))
