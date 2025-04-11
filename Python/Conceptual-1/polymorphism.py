73b8ed25e5f9164f6ca78a4f73034510d06cf60a
Idba1 <moniraislamself@gmail.com>
2025-04-12 00:50:39 +0600

    polimorphism

---
+++ Python/Conceptual-1/polymorphism.py
@@ -0,0 +1,44 @@
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        # super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+
+
+rect = Rectangle(4, 5)
+print(rect.area())
+print(rect.perimeter())
+
+circle = Circle(4)
+print(circle.area())
