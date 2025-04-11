cc7f94c015d7e9880cc849dd6a857a3409cd6a66
Idba1 <moniraislamself@gmail.com>
2025-04-12 00:42:06 +0600

    abstraction

---
+++ Python/Conceptual-1/abstraction.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        # super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+
+class Circle(Shape):
+    pass
+
+
+rect = Rectangle(4, 5)
+print(rect.area())
+print(rect.perimeter())
