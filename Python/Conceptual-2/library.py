6d51286beba65335b90c7c03e3c1a1602903e749
Idba1 <moniraislamself@gmail.com>
2025-04-12 01:43:56 +0600

    addBook, addUser, borrowedBook, returnBook

--- Python/Conceptual-2/library.py
+++ Python/Conceptual-2/library.py
@@ -11,7 +11,7 @@ class User:
         self.name = name
         self.password = password
         self.borrowedBooks = []  # empty list
-        self.returnBooks = []
+        self.returnedBooks = []
 
 
 class Library:
@@ -19,3 +19,60 @@ class Library:
         self.name = name
         self.users = []
         self.books = []
+
+
+
+    def addBook(self, id, name, q):
+        for book in self.books:
+            if book.id == id:
+                print("This Book already exists")
+                return
+        book = Book(self, id,  name, q)
+        self.books.append(book)
+        print(f"{book.name} added successfully")
+
+
+
+    def addUser(self, id, name, p):
+        for user in self.users:
+            if user.id == id:
+                print("This user already exists")
+                return
+        user = User(self, id,  name, p)
+        self.users.append(user)
+        print(f"{user.name} added successfully")
+
+
+
+    def borrowBook(self, user, BookId):
+        for book in self.books:
+            if book.id == BookId:
+                if book in user.borrowedBooks:
+                    print("Already Borrowed")
+                    return
+                elif book.quantity < 1:
+                    print("No available copies")
+                    return
+                else:
+                    user.borrowedBooks.append(book)
+                    book.quantity -= 1
+                    print("Borrowed Successfully")
+                    return
+        print(f"Not found any book with id : {BookId}")
+
+
+
+    def returnBook(self, user, BookId):
+        for book in self.books:
+            if book.id == BookId:
+                if book in user.borrowedBooks:
+                    book.quantity += 1
+                    user.borrowedBooks.remove(book)
+                    user.returnedBooks.append(book)
+                    return
+                else:
+                    print("You did not borrow this book")
+                    return
+        print(f"Not found any book with id : {BookId}")
+
+
