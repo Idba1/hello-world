1d3f0d8176685dea03e7591656659c6dc49bef6f
Idba1 <moniraislamself@gmail.com>
2025-04-11 21:02:24 +0600

    access modifier

---
+++ Python/Conceptual-1/encapsulation.py
@@ -0,0 +1,53 @@
+class Shop:
+
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, name, price):
+        product = Product(name, price)
+        self.products.append(product)
+        self.__balance = 500  # private attribute
+        self._owner = 'Mr. X'  # protected attribute
+
+    def get_balance(self):
+        return self.__balance
+
+    def __str__(self):  # first priority
+        return f"this is a shop with name {self.name}"
+
+    def __repr__(self):  # second priority
+        return "this is a shop"
+
+
+class SubShop(Shop):
+    pass
+
+
+class Product:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    # def __str__(self):
+    #     return self.name
+
+    def __repr__(self):
+        return self.name
+
+
+shop1 = Shop('test shop 1')
+shop1.add_product('Mobile phone', 40000)
+shop1.add_product('i phone', 140000)
+print(shop1)
+print(shop1.products)
+
+
+shop2 = Shop('test shop 2')
+shop2.add_product('Laptop', 10000000)
+shop2.add_product('monitor', 10000000)
+
+print(shop1.name)
+print(shop2.name)
+print(shop2.products)
+print(shop1.get_balance())
